951425730662e59022474f4858040998844eb185
ArnavPundir22 <arnavp128@gmail.com>
2026-09-07 14:59:57 +0530

    Support DB_PATH environment variable for persistent storage

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.join(__dirname, '..', 'glucotrack.db');
+const dbPath = process.env.DB_PATH || path.join(__dirname, '..', 'glucotrack.db');
 const sqlite = new sqlite3.Database(dbPath);
 
 export const db = {
